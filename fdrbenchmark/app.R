bbe2eff02094d829e4addfc926842f85cdf48ddf
kdkorthauer <kdkorthauer@gmail.com>
2019-03-29 09:50:13 -0400

    Add rest of case studies

--- fdrbenchmark/app.R
+++ fdrbenchmark/app.R
@@ -24,6 +24,8 @@ datasets <- as.list(datasets)
 
 possmethods <- candycols$Method[-which(candycols$Method %in% c("adapt-gam", "ashs"))]
 
+
+########### define categories of inputs
 # gwas 
 gwas <- datasets[casestudy == "GWAS"]
 names(gwas) <- ifelse(grepl("maf", gwas), "minor allele frequency", 
@@ -36,6 +38,41 @@ ds <- gsub("-csaw-[[:graph:]]+", "", chipseq)
 cov <- ifelse(grepl("cov", chipseq), "mean coverage",
               ifelse(grepl("uninf", chipseq), "uninformative", "region width"))
 names(chipseq) <- paste0(basename(ds), " & ", cov)
+
+
+# GSEA
+gsea <- datasets[casestudy == "GSEA"]
+ds <- ifelse(grepl("human", gsea), "human", "mouse")
+cov <- ifelse(grepl("uninf", gsea), "random", "gene set size")
+names(gsea) <- paste0(basename(ds), " & ", cov)
+
+
+# microbiome
+micro <- datasets[casestudy == "microbiome"]
+ds <- unlist(lapply(micro, function(x) basename(strsplit(x, "-")[[1]][1])))
+lev <- ifelse(grepl("otu", micro), "OTU", "genus")
+cov <- ifelse(grepl("mean|abun", micro), "mean abundance",
+              ifelse(grepl("uninf", micro), "random", 
+                     ifelse(grepl("log", micro), "log ubiquity", "ubiquity")))
+names(micro) <- paste0(basename(ds), " (", lev, ") & ", cov)
+
+# RNAseq
+rna <- datasets[casestudy == "RNAseq"]
+ds <- ifelse(grepl("brain", rna), "brain", "miRNA 200c")
+cov <- ifelse(grepl("uninf", rna), "random", "mean expression")
+names(rna) <- paste0(basename(ds), " & ", cov)
+
+# scRNAseq
+sc <- datasets[casestudy == "scRNAseq"]
+ds <- ifelse(grepl("human", sc), "human", "mouse")
+cov <- ifelse(grepl("det", sc), "detection rate",
+              ifelse(grepl("uninf", sc), "random", 
+                     "mean expression"))
+meth <-  ifelse(grepl("mast", sc), "MAST",
+                ifelse(grepl("scdd", sc), "scDD", 
+                       "Wilcox"))
+names(sc) <- paste0(basename(ds), " & ", cov, " (", meth, ")")
+
 
 # Define UI for application that draws key summary plots
 ui <- fluidPage(
@@ -61,6 +98,30 @@ ui <- fluidPage(
                 selectInput("dataset", "Dataset & Covariate:",
                             choices = chipseq,
                             selected = chipseq[1])
+            ),
+            conditionalPanel(
+                condition = "input.casestudy == 'GSEA'",
+                selectInput("dataset", "Dataset & Covariate:",
+                            choices = gsea,
+                            selected = gsea[1])
+            ),
+            conditionalPanel(
+                condition = "input.casestudy == 'microbiome'",
+                selectInput("dataset", "Dataset (Level) & Covariate:",
+                            choices = micro,
+                            selected = micro[1])
+            ),
+            conditionalPanel(
+                condition = "input.casestudy == 'RNAseq'",
+                selectInput("dataset", "Dataset & Covariate:",
+                            choices = rna,
+                            selected = rna[1])
+            ),
+            conditionalPanel(
+                condition = "input.casestudy == 'scRNAseq'",
+                selectInput("dataset", "Dataset & Covariate (Method):",
+                            choices = sc,
+                            selected = sc[1])
             ),
             checkboxGroupInput("methods",
                                "Methods",
